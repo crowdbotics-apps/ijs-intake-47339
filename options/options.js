144f2e89c605f5219e38033a58745a1faa832c00
Crowdbotics <team@crowdbotics.com>
2024-04-01 23:31:42 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ijs-intake-47339",
+  url: "https://ijs-intake-47339.botics.co",
+  api: "https://ijs-intake-47339.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
